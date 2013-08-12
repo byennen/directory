472c5a2264e9101361d9c47de63ce60fa151b6b2
Lance Ennen <byennen@gmail.com>
2013-08-12 16:28:36 -0500

    fixing broken tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ MetalsDirectory::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
 end
