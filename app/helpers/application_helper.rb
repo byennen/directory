b6fa31180fbf6d3fe9fe5c4ba8eb520e82650d97
Lance Ennen <byennen@gmail.com>
2013-11-13 00:47:48 -0500

    fixing badges and continue button colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,7 +30,7 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
     render(association.to_s.singularize + "_fields", f: builder)
   end
-  link_to(name, '#', class: "add_fields btn btn-success", data: {id: id, fields: fields.gsub("\n", "")})
+  link_to(name, '#', class: "add_fields btn btn-success", style: 'margin-left: 175px;', data: {id: id, fields: fields.gsub("\n", "")})
   end
 
   def nav_link(link_text, link_path)
