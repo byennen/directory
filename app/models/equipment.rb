517f3f3ff360b59a2fc2bf7e9ca28b228c25b695
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-12-13 13:26:18 +0700

    add sub_category model - conpany registration with sub-categories

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,6 @@
 class Equipment < ActiveRecord::Base
   has_many :categories, as: :categorizable
+  has_many :sub_categories, as: :sub_categorizable
   has_many :print_selections, :as => :printable
   has_many :online_selections, :as => :onlineable
 end
